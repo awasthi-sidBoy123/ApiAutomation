f906dc567733b2187213c6af1e2ba742d786a629
Devo <awasthisidboy123@gmail.com>
2021-03-05 19:00:56 +0530

    develop changed

--- src/test/java/Academy/AnExtentReports/InvokeTest.java
+++ src/test/java/Academy/AnExtentReports/InvokeTest.java
@@ -43,6 +43,7 @@ public class InvokeTest {
 		System.out.println("develop");
 		System.out.println("develop");
 		System.out.println("develop");
+		System.out.println("develop");
 		
 		extent.flush();
 	}
